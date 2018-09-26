d8c644166312565ea9b8825b349e85aa9b99dba3
Drew Terry <nick.terry@gmail.com>
2018-09-26 11:06:02 -0400

    .dotfiles to .setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-mkdir -p "$HOME/.dotfiles"
+mkdir -p "$HOME/.setup"
 
 curl -L https://github.com/drewterry/mac-setup/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'
 
-. "$HOME/.dotfiles/install.sh"
+. "$HOME/.setup/install.sh"
