e4045a4ae2ae3eb5764aa77f8a4e26692d9dd969
drew <drew.terry@skookum.com>
2018-09-28 09:47:22 -0400

    uncomment

--- install.sh
+++ install.sh
@@ -338,16 +338,16 @@ info "To begin, enter your password, to exit use Control-C"
 trap "safeExit" 2
 sudo -v
 
-# installCommandLineTools
-# installHomebrew
-# installBrewfile
+installCommandLineTools
+installHomebrew
+installBrewfile
 
-# installMacOSDefaults
+installMacOSDefaults
 installDotfiles
-# installRuby
-# installNode
-# configureSSH
-# syncVSCodeSettings
+installRuby
+installNode
+configureSSH
+syncVSCodeSettings
 
 sudo -k
 
